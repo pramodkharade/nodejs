e4da31c967e0afcc4d8a73e8f3d8c2339b86dcfe
pramodkharade <kharade.pramod91@gmail.com>
2019-04-20 15:55:18 +0530

    apend content in file

--- firstApp/app.js
+++ firstApp/app.js
@@ -1,2 +1,3 @@
 const fs = require('fs');
-fs.writeFileSync("notes.txt","This file was created by nodejs.My name is Pramod Kharade...");
+//fs.writeFileSync("notes.txt","This file was created by nodejs.My name is Pramod Kharade...");
+fs.appendFileSync('notes.txt','I am working with Blazeclan')

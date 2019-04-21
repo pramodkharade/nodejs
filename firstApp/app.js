3572347a9dc093d99ad873566abda29eed9ac0dc
pramodkharade <kharade.pramod91@gmail.com>
2019-04-21 13:39:35 +0530

    take commandline arugment using process object

--- firstApp/app.js
+++ firstApp/app.js
@@ -1,14 +1,12 @@
 const getNote = require('./utils');
-const validator = require('validator');
 const chalk = require('chalk');
-const fs = require('fs');
-//fs.writeFileSync("notes.txt","This file was created by nodejs.My name is Pramod Kharade...");
-fs.appendFileSync('notes.txt','I am working with Blazeclan');
-const msg = getNote();
-console.log('Message is:', msg);
-if(validator.isEmail('pramod@gmail.com')){
-    console.log(chalk.green('Validate Email address'));
-}else{
-    console.log(chalk.red('Invalidate Email address'));
+
+const command = process.argv[2];
+
+if(command === 'add'){
+    console.log('Adding Note!');
+}else if(command==='remove'){
+    console.log('Removing note!');
 }
+console.log(process.argv);
 

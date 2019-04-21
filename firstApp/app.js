bcc881e6eb159a7fb1ae97dac23cdc3367d078ca
pramodkharade <kharade.pramod91@gmail.com>
2019-04-21 12:07:47 +0530

    chalk lib

--- firstApp/app.js
+++ firstApp/app.js
@@ -1,8 +1,14 @@
 const getNote = require('./utils');
 const validator = require('validator');
+const chalk = require('chalk');
 const fs = require('fs');
 //fs.writeFileSync("notes.txt","This file was created by nodejs.My name is Pramod Kharade...");
 fs.appendFileSync('notes.txt','I am working with Blazeclan');
 const msg = getNote();
 console.log('Message is:', msg);
-console.log(validator.isEmail('pramod@@gmail.com'));
+if(validator.isEmail('pramod@gmail.com')){
+    console.log(chalk.green('Validate Email address'));
+}else{
+    console.log(chalk.red('Invalidate Email address'));
+}
+

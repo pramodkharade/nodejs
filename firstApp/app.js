4d294e34fe24a028144bd9a917ae1e4e53749feb
pramod.kharade <pramod.kharade@blazeclan.com>
2019-04-22 16:14:34 +0530

    commandline custom command with help of yargs lib

--- firstApp/app.js
+++ firstApp/app.js
@@ -1,12 +1,41 @@
 const getNote = require('./utils');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
 const command = process.argv[2];
+//create add command
+yargs.command({
+    command:'add',
+    describe:' Add a new note!',
+    handler: function(){
+        console.log('Adding new note!');
+    }
+});
 
-if(command === 'add'){
-    console.log('Adding Note!');
-}else if(command==='remove'){
-    console.log('Removing note!');
-}
-console.log(process.argv);
+//create remove command
+yargs.command({
+    command:'remove',
+    describe:' remove a new note!',
+    handler: function(){
+        console.log('remove new note!');
+    }
+});
+
+//create read command
+yargs.command({
+    command:'read',
+    describe:' read a new note!',
+    handler: function(){
+        console.log('read new note!');
+    }
+});
+//create list command
+yargs.command({
+    command:'list',
+    describe:' list a new note!',
+    handler: function(){
+        console.log('list new note!');
+    }
+});
+console.log(yargs.argv);
 

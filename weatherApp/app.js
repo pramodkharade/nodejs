4b4e6d3b63fa38189763d348e0c1f0887b66cf38
pramod.kharade <pramod.kharade@blazeclan.com>
2019-04-24 11:05:06 +0530

    with json option property setting. no need of json parse

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -2,7 +2,10 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/886527ebb03683c89fca0634ee21c6d7/18.5348336,73.8477759';
 
-request({url:url},(error,response)=>{
-    const data = JSON.parse(response.body);
+request({
+    url:url,
+    json:true
+  },(error,response)=>{
+    const data = response.body;
     console.log(data.currently);
 });

00f81fe6914ffbc5f3ab8c9ceeba3c080bdd9fed
pramodkharade <kharade.pramod91@gmail.com>
2019-04-27 01:32:22 +0530

    summary of day

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -8,4 +8,5 @@ request({
   },(error,response)=>{
     const data = response.body;
     console.log('It is currently',data.currently.temperature,' degree out. There is a ',data.currently.precipProbability,' % chance of rain.');
-});
+    console.log(response.body.daily.data[0].summary);
+  });

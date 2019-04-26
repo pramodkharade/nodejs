bdc4c61993f0bf170f9f02af450000c20aa1736e
pramodkharade <kharade.pramod91@gmail.com>
2019-04-27 01:15:18 +0530

    temperature in celcius

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,11 +1,11 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/886527ebb03683c89fca0634ee21c6d7/18.5348336,73.8477759';
+const url = 'https://api.darksky.net/forecast/886527ebb03683c89fca0634ee21c6d7/18.5348336,73.8477759?units=si';
 
 request({
     url:url,
     json:true
   },(error,response)=>{
     const data = response.body;
-    console.log(data.currently);
+    console.log('It is currently',data.currently.temperature,' degree out. There is a ',data.currently.precipProbability,' % chance of rain.');
 });

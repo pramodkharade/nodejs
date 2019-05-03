94529307e4636c8c45a1ac563d28e45cfaa201e5
pramodkharade <kharade.pramod91@gmail.com>
2019-05-03 06:38:26 +0530

    setup web server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,5 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('',(req,res)=>{
+    res.send('Hello Express');
+});
 
+app.listen(3000,()=>{
+    console.log('Server is running on 3000 ');
 });

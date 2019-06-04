dc77ed21d64519d5d7405982312ce0599ac82738
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-04 16:07:22 +0530

    Fetch API and promises interface provided by JavaScript

--- web-server/public/js/custom.js
+++ web-server/public/js/custom.js
@@ -1,1 +1,11 @@
 console.log('Public assets js loaded!');
+fetch('http://localhost:3000/weather?address=mumbai').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log('Locations:', data.locations);
+            console.log('Forecast:', data.forecast.summary);
+        }
+    });
+});

05bf2f4bc01274c3a130cf7875871c42639cd2d2
pramod.kharade <pramod.kharade@blazeclan.com>
2019-06-04 16:49:58 +0530

    Search form and forecast api search

--- web-server/public/js/custom.js
+++ web-server/public/js/custom.js
@@ -1,5 +1,10 @@
 console.log('Public assets js loaded!');
-fetch('http://localhost:3000/weather?address=mumbai').then((response) => {
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const location = search.value;
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             console.log(data.error);
@@ -8,4 +13,6 @@ fetch('http://localhost:3000/weather?address=mumbai').then((response) => {
             console.log('Forecast:', data.forecast.summary);
         }
     });
+});
+    
 });
